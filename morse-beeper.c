f119860a825ba897044777af0fb353fad738bf87
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2017-08-01 18:25:16 +0200

    Trying to beep

--- morse-beeper.c
+++ morse-beeper.c
@@ -3,12 +3,29 @@
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <asm/io.h>
+#include <linux/kd.h>
+#include <linux/kmod.h>
+//#include <sys/types.h>
+//#include <sys/stat.h>
+#include "/usr/include/asm-generic/fcntl.h"
 
 MODULE_LICENSE("MPL-2.0");
 MODULE_AUTHOR("Jaroslaw Wiosna");
 MODULE_DESCRIPTION("Morse Beeper");
 
 irq_handler_t irq_handler (int irq, void *dev_id, struct pt_regs *regs) {
+    char* argv[4];
+    char* envp[4]; 
+    argv[0] = "/bin/bash";
+    argv[1] = "-c";
+    argv[2] = "play -n synth 0.1 sine 400 vol 0.5";
+    argv[3] = NULL;
+
+    envp[0] = "HOME=/";
+    envp[1] = "TERM=linux";
+    envp[2] = "PATH=/sbin:/usr/sbin:/bin:/usr/bin";
+    envp[3] = NULL;
+
     static unsigned char scancode;
     scancode = inb(0x60);
     if ((scancode == 0x01) || (scancode == 0x81)) {
@@ -18,6 +35,7 @@ irq_handler_t irq_handler (int irq, void *dev_id, struct pt_regs *regs) {
     } else if (scancode == 0x3c) {
         printk("You pressed F2\n");
     } else if (scancode == 0x3d) {
+        call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
         printk("You pressed F3\n");
     } else if (scancode == 0x3e) {
         printk("You pressed F4\n");
